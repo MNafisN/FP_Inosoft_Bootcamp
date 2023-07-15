57e496265d4d7fcdbd644231a3b49c07dd4a1b3c
bayuuat <bayuuat@gmail.com>
2023-07-15 10:11:21 +0700

    feat(login): add login page and middleware

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Login from '../pages/Login.vue'
 import Home from '../pages/Home.vue'
 import CreateInstruction from '../pages/CreateInstruction.vue'
 import EditInstruction from '../pages/EditInstruction.vue'
 import DetailInstruction from '../pages/DetailInstruction.vue'
 
 const routes = [
+    {
+        path: '/app/login',
+        component: Login,
+        meta:{
+            guestOnly: true
+        }
+    },
     {
         path: '/app',
         component: Home
@@ -26,6 +34,25 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+const checkIfUserIsAuthenticated = () => {
+    const accessToken = localStorage.getItem('access_token')
+    return !!accessToken
+}
+
+router.beforeEach((to, from, next) => {
+    const guestOnly = to.matched.some((route) => route.meta.guestOnly)
+
+    const isAuthenticated = checkIfUserIsAuthenticated()
+
+    if (!guestOnly & !isAuthenticated) {
+        next('app/login')
+    } else if(guestOnly & isAuthenticated){
+        next('app')
+    } else {
+        next()
+    }
 })
 
 export default router
